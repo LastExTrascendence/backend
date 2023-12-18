03efea3c189ceab6f99d2103916703799d5fffb2
jusohn <jusohn@student.42seoul.kr>
2023-12-18 21:17:29 +0900

    fix: deleted_at 와 access_token 에 nullable 추가

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,41 +1,49 @@
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+} from "typeorm";
 import { Status } from "./user.enum";
 
 @Entity()
-export class User extends BaseEntity{
-    @PrimaryGeneratedColumn()
-    id: number;
-    
-    @Column()
-    intra_id: string;
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    nickname: string;
+  @Column()
+  @Unique(["intra_id"])
+  intra_id: string;
 
-    @Column()
-    avatar : string;
-    
-    @Column()
-    status : Status
+  @Column()
+  @Unique(["nickname"])
+  nickname: string;
 
-    @Column()
-    email: string;
-    
-    @Column()
-    "2fa_status": boolean;
+  @Column()
+  avatar: string;
 
-    @Column()
-    created_at: Date;
+  @Column()
+  email: string;
 
-    @Column()
-    deleted_at: Date;
-    
-    @Column()
-    access_token: string;
+  @Column()
+  "2fa_status": boolean;
 
+  @Column()
+  status: Status;
 
-    // @OneToMany(type => Board, board => board.user, {eager:true})
-    // boards: Board[]
+  @Column()
+  created_at: Date;
+
+  @Column({ nullable: true })
+  deleted_at: Date;
+
+  @Column({ nullable: true })
+  access_token: string;
+
+  // @OneToMany(type => Board, board => board.user, {eager:true})
+  // boards: Board[]
 }
 
 //@Unique(['username'])
