2d8efc19cc76e8eef3d4dff972d99222e78da9a5
jusohn <jusohn@student.42seoul.kr>
2023-12-20 16:37:36 +0900

    fix: 새로운 User 테이블에 맞춰 dto 수정 #16

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -12,22 +12,20 @@ export class UserDto {
   nickname: string;
   avatar: string;
   email: string;
-  "2fa_status": boolean;
+  two_fa: boolean;
   status: Status;
 }
 
 /**
  * @description JWT 토큰 내 유저 세션 정보를 담은 DTO
  *
- * @param {number} oauth_id - 유저의 고유 42 ID
- * @param {string} oauth_name - 유저의 고유 42 Intra ID
+ * @param {string} intra_name - 유저의 고유 42 Intra ID
  * @param {number} iat - JWT 발급 시간
  * @param {number} ext - JWT 만료 시간
  * @extends {UserDto}
  */
 export class UserSessionDto extends UserDto {
-  // oauth_id: number;
-  oauth_name: string;
+  intra_name: string;
   iat?: number;
   ext?: number;
 }
