26c9250b7c42f3071014093aa921f512d3f5a956
jusohn <jusohn@student.42seoul.kr>
2024-01-20 16:52:15 +0900

    fix: Query 용량 상향

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { initializeTransactionalContext } from "typeorm-transactional";
 import * as cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 import * as config from "config";
+import { json, urlencoded } from "express";
 
 async function bootstrap() {
   dotenv.config(); // .env 파일 로드
@@ -18,6 +19,8 @@ async function bootstrap() {
     preflightContinue: false,
   });
   app.use(cookieParser());
+  app.use(json({ limit: "2mb" }));
+  app.use(urlencoded({ limit: "2mb", extended: true }));
   await app.listen(config.get("BE").get("port"));
 }
 bootstrap();
