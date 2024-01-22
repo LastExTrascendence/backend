65fbd0d697da3d51d7b6bfec693f20c4e6c5cc81
jusohn <jusohn@student.42seoul.kr>
2024-01-22 20:39:21 +0900

    fix: CORS 를 위해 Whitelist 추가

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,33 @@ import * as cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 import * as config from "config";
 import { json, urlencoded } from "express";
+import { ImATeapotException } from "@nestjs/common";
+
+const whitelist = [
+  `http://${config.get("FE").get("domain")}:${config.get("FE").get("port")}`,
+  `http://${config.get("FE").get("domain")}`,
+  `http://localhost:${config.get("FE").get("port")}`,
+  `http://localhost`,
+];
 
 async function bootstrap() {
   dotenv.config(); // .env 파일 로드
   initializeTransactionalContext();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: `http://${config.get("FE").get("domain")}:${config
-      .get("FE")
-      .get("port")}`,
+    origin: function (origin, callback) {
+      if (!origin) {
+        callback(null, true);
+        return;
+      }
+      if (whitelist.includes(origin) || !!origin.match(/localhost$/)) {
+        // console.log("allowed cors for:", origin);
+        callback(null, true);
+      } else {
+        // console.log("blocked cors for:", origin);
+        callback(new ImATeapotException("Not allowed by CORS"), false);
+      }
+    },
     credentials: true,
     preflightContinue: false,
   });
